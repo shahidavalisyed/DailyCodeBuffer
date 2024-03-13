07602aaab4594d3ffd64f7c5ebd4038de14b6011
Shahidavali Syed <syedshahidavali@gmail.com>
2024-03-13 07:33:09 +0530

    Test DepartmentServiceApplication

--- department-service/src/main/java/com/dailycodebuffer/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/DepartmentServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
+	//hhh
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
