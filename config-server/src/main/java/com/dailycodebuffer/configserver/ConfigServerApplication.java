7ed7c93e1b35666b560f4e4c97c1b867d35ab934
Shahidavali Syed <syedshahidavali@gmail.com>
2024-03-17 17:58:39 +0530

    from tuto

--- config-server/src/main/java/com/dailycodebuffer/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/dailycodebuffer/configserver/ConfigServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigServerApplication {
 
+	//From tutorial
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
